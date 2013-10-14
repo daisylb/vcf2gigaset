7ae5646eeb6095d616a13e006624f42acea601f1
Adam Brenecki <adam@brenecki.id.au>
2013-10-14 17:49:08 +1030

    Updated spacing and encoding

--- vcf2gigaset.rb
+++ vcf2gigaset.rb
@@ -7,11 +7,11 @@ cards.each do |card|
   card.telephones.each do |phone|
     name = "#{card.name.fullname} #{phone.location.join ""} #{phone.nonstandard.join ""}"
     formatted_phone = phone.gsub %r{[^0-9\+\*\#]+}, ""
-    puts <<-EOS.outdent
+    puts <<-EOS.outdent.gsub("\n", "\r\n").encode "iso-8859-1"
       BEGIN:VCARD
       VERSION:2.1
-      FN:#{name}
-      N:#{name}
+      FN: #{name}
+      N: #{name}
       TEL;HOME:#{formatted_phone}
       END:VCARD
 
